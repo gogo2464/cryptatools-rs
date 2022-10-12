cb0f907cfdbab89badcfa03918c46ae06c51c721
gogo <gogo246475@gmail.com>
2022-10-12 18:53:33 +0200

    Set infinite key length for caesar number algo encryption.

--- cryptatools-core/src/cryptography/encryption/monoalphabetic_ciphers/caesar_number.rs
+++ cryptatools-core/src/cryptography/encryption/monoalphabetic_ciphers/caesar_number.rs
@@ -58,11 +58,11 @@ impl CaesarNumberAlgorithm {
      ///  assert_eq!(re_encrypted, "KKK");
      ///  ```
 
-     pub fn encrypt(&self, plain_text: Vec<u8>, key: u32) -> Vec<u8> {
+     pub fn encrypt<T>(&self, plain_text: Vec<u8>, key: usize) -> Vec<u8> {
         let mut result: Vec<u8> = Vec::new();
 
         for element in plain_text {
-            let character: u8 = (element + key as u8) % self.alphabet.len() as u8; //TODO: set key and alphabet len as usize for infinite len
+            let character: u8 = ((element as usize + key) % self.alphabet.len()) as u8;
             result.push(character);
         }
 
