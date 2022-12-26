a8cc20e5187655cecd569f6ffb21df78209384c6
gogo2464 <57051187+gogo2464@users.noreply.github.com>
2022-12-26 23:18:47 +0000

    Deploying to gh-pages from @ gogo2464/cryptatools-rs@0cdd8ced2bc022aeadb0c63d8d197c6cdd950c2e 🚀

--- implementors/serde/ser/trait.Error.js
+++ implementors/serde/ser/trait.Error.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
-"bincode":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.151/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"type\" href=\"bincode/type.Error.html\" title=\"type bincode::Error\">Error</a>"]],
+"bincode":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.152/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"type\" href=\"bincode/type.Error.html\" title=\"type bincode::Error\">Error</a>"]],
 "serde":[],
 "serde_json":[["impl <a class=\"trait\" href=\"serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/struct.Error.html\" title=\"struct serde_json::Error\">Error</a>"]],
-"toml":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.151/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"toml/ser/enum.Error.html\" title=\"enum toml::ser::Error\">Error</a>"]]
+"toml":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.152/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"toml/ser/enum.Error.html\" title=\"enum toml::ser::Error\">Error</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
