b6ffc3a2d16d76255cc16d2fdcefafe98092c01f
gogo <gogo246475@gmail.com>
2023-03-08 10:18:26 +0100

    Add coincidence index guesser python bindings.

--- cryptatools-core/src/lib.rs
+++ cryptatools-core/src/lib.rs
@@ -5,5 +5,6 @@ pub mod utils;
 use crate::utils::alphabets::{Encoding, Alphabet, split_bytes_by_characters_representation, uniffy_opcode_group};
 use crate::cryptography::classical::encryption::monoalphabetic_ciphers::caesar_number::CaesarNumberAlgorithm;
 use crate::cryptography::classical::encryption::transpositional_ciphers::columnar_transposition::ColumnarTranspositionAlgorithm;
+use crate::cryptanalysis::general_cryptanalysis_methods::frequency_analysis::coincidence_index::CoincidenceIndexGuesser;
 
 uniffi_macros::include_scaffolding!("cryptatools");
